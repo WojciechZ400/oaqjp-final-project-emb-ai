fac82f84b6a884c4f943f13606313bd09a78d1fd
WojciechZ400 <wojciech.zub2@wp.pl>
2026-03-08 19:12:59 -0400

    Added Tasks 5, 6, 7

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -13,32 +13,38 @@ def emotion_detector(text_to_analyze):
 
     formatted_response = json.loads(response.text)
 
+    if response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dominant_emotion_name = None
+    else:
+        anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        dominant_emotion_name = "anger"
+        max_score = anger_score
 
-    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    dominant_emotion_name = "anger"
-    max_score = anger_score
+        disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        if max_score < disgust_score:
+            dominant_emotion_name = "disgust"
+            max_score = disgust_score
 
-    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    if max_score < disgust_score:
-        dominant_emotion_name = "disgust"
-        max_score = disgust_score
+        fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        if max_score < fear_score:
+            dominant_emotion_name = "fear"
+            max_score = fear_score
 
-    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    if max_score < fear_score:
-        dominant_emotion_name = "fear"
-        max_score = fear_score
+        joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        if max_score < joy_score:
+            dominant_emotion_name = "joy"
+            max_score = joy_score
 
-    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    if max_score < joy_score:
-        dominant_emotion_name = "joy"
-        max_score = joy_score
+        sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+        if max_score < sadness_score:
+            dominant_emotion_name = "sadness"
+            max_score = sadness_score
 
-    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
-    if max_score < sadness_score:
-        dominant_emotion_name = "sadness"
-        max_score = sadness_score
-
-    x = max(5, 10)
     return {'anger': anger_score,
 'disgust': disgust_score,
 'fear': fear_score,
